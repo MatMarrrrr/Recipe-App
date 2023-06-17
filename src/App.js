dec296320b5d3a62661395cc7a392350bcc6559b
MatMarrr <mateuszrmarek@gmail.com>
2023-06-18 00:31:09 +0200

    Live version update

--- src/App.js
+++ src/App.js
@@ -3,57 +3,62 @@ import Recipe from "./Recipe";
 import "./App.css";
 
 const App = () => {
-
-  const APP_ID = "9d08158b";
-  const APP_KEY = "9f913d708f562a6afd581668801ed6c1";
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     getRecipes();
   }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipes(data.hits);
-  }
+  };
 
-  const updateSearch = e => {
+  const updateSearch = (e) => {
     setSearch(e.target.value);
-  }
+  };
 
-  const getSearch = e => {
+  const getSearch = (e) => {
     e.preventDefault();
     setQuery(search);
     setSearch("");
-  }
+  };
 
   return (
     <div className="App">
-
       <div className="search-engine-title-box">
         <div className="search-engine-title">Recipe Search Engine</div>
       </div>
 
       <form onSubmit={getSearch} className="search-form">
-
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
 
         <button className="search-button" type="submit">
           Search
         </button>
-
       </form>
 
       <div className="search-engine-query-box">
-        <div className="search-engine-query-text">Showing result for: {query}</div>
+        <div className="search-engine-query-text">
+          Showing result for: {query}
+        </div>
       </div>
 
       <div className="recipes">
-        {recipes.map(recipe => (
+        {recipes.map((recipe) => (
           <Recipe
             key={recipe.recipe.label}
             title={recipe.recipe.label}
@@ -63,8 +68,7 @@ const App = () => {
           />
         ))}
       </div>
-
     </div>
-  )
-}
+  );
+};
 export default App;
